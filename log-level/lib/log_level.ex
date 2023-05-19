5c4bfa1fc8e579e76150fb2288c4a8d656ab3eb2
Joe Sacco <102517425+joe-sacco@users.noreply.github.com>
2023-05-19 19:00:13 +0900

    feat: Update log_level.ex

--- log-level/lib/log_level.ex
+++ log-level/lib/log_level.ex
@@ -5,11 +5,27 @@ defmodule LogLevel do
     if Enum.at(log_label_list, log_code) in log_label_list == false do
       :unknown
     else
+    if legacy? == false and log_code == 0 do
+      :unknown
+    end
+    if legacy? == false and log_code == 5 do
+      :unknown
+    end
       Enum.at(log_label_list, log_code)
     end
   end
 
-  def alert_recipient(level, legacy?) do
-    # Please implement the alert_recipient/2 function
+  def alert_recipient(log_code, legacy?) do
+    # recipients = [:ops, :dev1, :dev2]
+    # IO.puts("🟡🟡🟡🟡🟡🟡🟡🟡", LogLevel.to_label(log_code, legacy?))
+    if LogLevel.to_label(log_code, legacy?) == :error or LogLevel.to_label(log_code, legacy?) == :fatal do
+      :ops
+    end
+    if LogLevel.to_label(log_code, legacy?) == :uknown and legacy? == true do
+      :dev1
+    end
+    if LogLevel.to_label(log_code, legacy?) == :uknown and legacy? == false do
+      :dev2
+    end
   end
 end
