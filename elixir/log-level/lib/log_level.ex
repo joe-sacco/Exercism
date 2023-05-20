6f594b95d20449feed0caf04b1fb96f621804388
joe-sacco <102517425+joe-sacco@users.noreply.github.com>
2023-05-20 19:29:14 +0900

    feat: Add log_level.ex

--- elixir/log-level/lib/log_level.ex
+++ elixir/log-level/lib/log_level.ex
@@ -2,30 +2,21 @@ defmodule LogLevel do
   def to_label(log_code, legacy?) do
     log_label_list = [:trace, :debug, :info, :warning, :error, :fatal, :unknown]
     log_label = Enum.at(log_label_list, log_code)
-    if Enum.at(log_label_list, log_code) in log_label_list == false do
-      :unknown
-    else
-    if legacy? == false and log_code == 0 do
-      :unknown
-    end
-    if legacy? == false and log_code == 5 do
-      :unknown
-    end
-      Enum.at(log_label_list, log_code)
+    cond do
+      log_code > 5 -> :unknown
+      legacy? == true and log_code == 0 -> :unknown
+      legacy? == true and log_code == 5 -> :unknown
+      true -> log_label
     end
   end
 
   def alert_recipient(log_code, legacy?) do
-    # recipients = [:ops, :dev1, :dev2]
-    # IO.puts("🟡🟡🟡🟡🟡🟡🟡🟡", LogLevel.to_label(log_code, legacy?))
-    if LogLevel.to_label(log_code, legacy?) == :error or LogLevel.to_label(log_code, legacy?) == :fatal do
-      :ops
-    end
-    if LogLevel.to_label(log_code, legacy?) == :uknown and legacy? == true do
-      :dev1
-    end
-    if LogLevel.to_label(log_code, legacy?) == :uknown and legacy? == false do
-      :dev2
+    label = LogLevel.to_label(log_code, legacy?)
+    cond do
+      label == :error or label == :fatal -> :ops
+      legacy? == true and label == :unknown -> :dev1
+      log_code > 5 -> :dev2
+      true -> false
     end
   end
 end
